1fbef0412d72314048909c1cf706bc9f4cbdfe57
rhperera <harith9127@gmail.com>
2021-08-07 18:56:23 +0530

    Add docker file

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -9,7 +9,7 @@ export class Config {
 
     private constructor() {
         dotenv.config({path: '.env'});
-        this.PORT = '8090' || '8080';
+        this.PORT = '8080';
         this.REDIS_HOST_IP = process.env.REDIS_HOST_IP || '127.0.0.1';
         this.FIXER_API_KEY = process.env.FIXER_API_KEY || 'API_KEY';
         this.FIXER_END_POINT = process.env.FIXER_END_POINT || '127.0.0.1';
