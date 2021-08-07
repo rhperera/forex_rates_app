4d9ea98d0dd8ae51f4f9e92d7c02371e2f443c0e
rhperera <harith9127@gmail.com>
2021-08-07 22:20:48 +0530

    Add docker compose

--- start.sh
+++ start.sh
@@ -4,5 +4,4 @@ touch ~/logs/app.log
 cp cacheUpdater.sh ~/cacheUpdater.sh
 chmod 755 cacheUpdater.sh
 crontab -l | { cat; echo "0 */1 * * *  ~/cacheUpdater.sh"; } | crontab -
-docker build . -t node/forex
-docker run -p 8080:8080 node/forex
+docker-compose up
