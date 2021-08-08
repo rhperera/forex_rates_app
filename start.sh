a4001f8637a94edcef35a120f04c498818b69593
rhperera <harith9127@gmail.com>
2021-08-08 13:38:01 +0530

    Fix cron time

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cp cacheUpdater.sh ~/cacheUpdater.sh
 chmod 755 ~/cacheUpdater.sh
-crontab -l | { cat; echo "* */1 * * *  ~/cacheUpdater.sh"; } | crontab -
+crontab -l | { cat; echo "0 */1 * * *  ~/cacheUpdater.sh"; } | crontab -
 docker-compose up --build
